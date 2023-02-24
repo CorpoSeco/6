420af04804da21341f4e855c6d11c2bd52c165b9
TheAtt1la <alittauwu@gmail.com>
2023-02-23 21:34:51 -0300

    add

---
+++ alpha.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+ALPHA_MAINTAINER := TheAtt1la
+TARGET_ENABLE_BLUR := true
+TARGET_CORE_GAPPS := true
+WITH_GAPPS := true
